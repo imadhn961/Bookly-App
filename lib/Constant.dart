d4bd7ce24914d6cdfc3210a556182964d97a9ccf
imadhn961 <imad.hassan.0961@gmail.com>
2025-12-07 18:04:46 +0200

    Handling Exceptions

--- lib/Constant.dart
+++ lib/Constant.dart
@@ -3,4 +3,3 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff100B20);
 const KtransitionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = 'GT Sectra Fine';
-const KGoogleApi ='AIzaSyCtZzBNBU1T38TAq4D2qHv7HxBJVCBCxkU';
